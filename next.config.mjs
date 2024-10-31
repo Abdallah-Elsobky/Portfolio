768fc91380f7de3e72cc8f30d9d9179fc2e4cebb
Abdallah Elsobky <abdullah442286@fsc.bu.edu.eg>
2024-10-31 13:08:42 +0300

    update firebase to be automatic deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(mp3|wav)$/i,
